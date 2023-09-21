f61c4b77f8fbfcd74785531a68cecade3f471807
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2023-09-20 21:13:11 -0400

    Update ScoutForm.tsx

--- src/views/Scout/ScoutForm.tsx
+++ src/views/Scout/ScoutForm.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Box, Button, Divider, Grid, Tab, Tabs, TextField, ToggleButton, ToggleButtonGroup, MenuItem, Select, FormControl,  InputLabel} from '@mui/material'
 import { useEffect, useState } from 'react'
-import { Link, Route, redirect } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import { addReport } from 'db/connector'
 import ScoringTable from './ScoringTable'
 
@@ -236,7 +236,7 @@ export default function ScoutForm() {
 
     console.log(body)
     await addReport(body)
-    return redirect ('/reports')
+    alert('Report submitted!')
   }
 
   const [value, setValue] = React.useState(0);
